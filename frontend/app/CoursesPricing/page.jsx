7282e1aabb8bf7bf12fac91153eba0c8698ac30d
amandeepkumarflymediatech-star <amandeepkumar.flymediatech@gmail.com>
2026-01-19 18:46:15 +0530

    add suspense

--- frontend/app/CoursesPricing/page.jsx
+++ frontend/app/CoursesPricing/page.jsx
@@ -1,7 +1,18 @@
 "use client";
 
+import { Suspense } from "react";
 import CoursesPricing from "@/components/CoursesPricing";
 
-export default function DashboardPage() {
-  return <CoursesPricing />;
+export default function CoursesPage() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <CoursesPricing />
+    </Suspense>
+  );
 }
+
+const Loading = () => (
+  <section className="py-20 text-center bg-[#F8F3F3]">
+    <p className="text-gray-600">Loading pricing...</p>
+  </section>
+);
